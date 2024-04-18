25bc60c930dfd1042b25db7d23a3a17faf7c9fef
Alex Shrestha <alexshrestha1996@gmail.com>
2024-04-18 13:03:12 -0700

    added memoization solution

--- dp/cutrod.py
+++ dp/cutrod.py
@@ -14,14 +14,35 @@ def cut_rod(p, n):
     return q
 
 
+def memoized_cut_rod(p, n):
+    # where solutions will be remembered
+    r = [-1] * (n+1)
+
+    return memoized_cut_rod_helper(p, n, r)
+
+def memoized_cut_rod_helper(p, n, r):
+    if r[n] >= 0:
+        return r[n]
+
+    if n == 0:
+        q = 0
+    else:
+        q = -1
+
+        for i in range(1, n+1):
+            q = max(q, p[i] + memoized_cut_rod_helper(p, n-i, r))
+
+    r[n] = q
+    return q
+
+
 def main():
     p = [0, 1, 5, 8, 9, 10, 17, 18, 20]
     
 
     for i in range(1, 8):
-        max_profit = cut_rod(p, i)
+        max_profit = memoized_cut_rod(p, i)
         print(f"Max profit for {i}: ${max_profit}")
-
 
     return
 
