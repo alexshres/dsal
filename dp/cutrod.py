7ee459d0bd3cfc85eda945b7a50aaa7623cdad76
Alex Shrestha <alexshrestha1996@gmail.com>
2024-04-19 11:41:42 -0700

    added tabulation solution to cutrod

--- dp/cutrod.py
+++ dp/cutrod.py
@@ -35,14 +35,35 @@ def memoized_cut_rod_helper(p, n, r):
     r[n] = q
     return q
 
+def tabulation_cut_rod(p, n):
+    r = [-1] * (n+1)
+    r[0] = 0
+
+    for j in range(1, n+1):
+        q = -1
+
+        for i in range(1, j+1):
+            q = max(q, p[i] + r[j-i])
+
+        r[j] = q
+
+    return r[n]
+
 
 def main():
     p = [0, 1, 5, 8, 9, 10, 17, 18, 20]
     
 
+    print("Using memoization:")
     for i in range(1, 8):
         max_profit = memoized_cut_rod(p, i)
         print(f"Max profit for {i}: ${max_profit}")
+
+    print("Using tabulation:")
+    for i in range(1, 8):
+        max_profit = tabulation_cut_rod(p, i)
+        print(f"Max profit for {i}: ${max_profit}")
+
 
     return
 
